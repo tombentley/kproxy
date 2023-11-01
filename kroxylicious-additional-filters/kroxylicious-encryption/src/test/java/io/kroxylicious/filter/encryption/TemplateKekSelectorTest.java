2eb98d3bdcbc6b3e018824bdc12d0bafc3d9c5c9
Tom Bentley <tbentley@redhat.com>
2023-11-01 20:01:09 +1300

    KW comments, and more doc
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-additional-filters/kroxylicious-encryption/src/test/java/io/kroxylicious/filter/encryption/TemplateKekSelectorTest.java
+++ kroxylicious-additional-filters/kroxylicious-encryption/src/test/java/io/kroxylicious/filter/encryption/TemplateKekSelectorTest.java
@@ -6,7 +6,6 @@
 
 package io.kroxylicious.filter.encryption;
 
-import java.util.ServiceLoader;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
@@ -14,7 +13,6 @@ import org.junit.jupiter.api.Test;
 
 import io.kroxylicious.kms.provider.kroxylicious.inmemory.InMemoryKms;
 import io.kroxylicious.kms.provider.kroxylicious.inmemory.InMemoryKmsService;
-import io.kroxylicious.kms.service.KmsService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -28,17 +26,10 @@ class TemplateKekSelectorTest {
         assertEquals("Unknown template parameter: topicId", e.getMessage());
     }
 
-    private static InMemoryKmsService createServiceInstance() {
-        return (InMemoryKmsService) ServiceLoader.load(KmsService.class).stream()
-                .filter(p -> p.type() == InMemoryKmsService.class)
-                .findFirst()
-                .get()
-                .get();
-    }
 
     @Test
     void shouldResolveWhenAliasExists() throws ExecutionException, InterruptedException {
-        InMemoryKms kms = createServiceInstance().buildKms(new InMemoryKmsService.Config(12, 128));
+        InMemoryKms kms = InMemoryKmsService.newInstance().buildKms(new InMemoryKmsService.Config(12, 128));
         var selector = new TemplateKekSelector<>(kms, "topic-${topicName}");
 
         var kek = kms.generateKey();
@@ -49,7 +40,7 @@ class TemplateKekSelectorTest {
 
     @Test
     void shouldThrowWhenAliasDoesNotExist() throws ExecutionException, InterruptedException {
-        InMemoryKms kms = createServiceInstance().buildKms(new InMemoryKmsService.Config(12, 128));
+        InMemoryKms kms = InMemoryKmsService.newInstance().buildKms(new InMemoryKmsService.Config(12, 128));
         var selector = new TemplateKekSelector<>(kms, "topic-${topicName}");
 
         var map = selector.selectKek(Set.of("my-topic")).toCompletableFuture().get();
