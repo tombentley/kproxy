9fb980399528f35fee6278d8e785a863553c9cbc
Tom Bentley <tbentley@redhat.com>
2025-07-07 18:07:49 +1200

    Alternative ClientSubjectAware API
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-api/src/main/java/io/kroxylicious/proxy/authentication/ClientSubjectAware.java
+++ kroxylicious-api/src/main/java/io/kroxylicious/proxy/authentication/ClientSubjectAware.java
@@ -6,28 +6,65 @@
 
 package io.kroxylicious.proxy.authentication;
 
-import javax.security.auth.Subject;
-import javax.security.auth.login.LoginException;
+import javax.security.auth.x500.X500Principal;
 
 import io.kroxylicious.proxy.filter.Filter;
 
+import edu.umd.cs.findbugs.annotations.Nullable;
+
 /**
- * This interface may be implemented by {@link Filter}s to learn about client authentication outcomes.
+ * <p>This interface may be implemented by {@link Filter}s to learn about client authentication outcomes.</p>
+ *
+ * <p>The runtime guarantees that <em>eventually</em> either:
+ * <ul>
+ * <li>both {@link #onClientTlsAuthentication(X500Principal, ClientAuthenticationContext)}
+ * and {@link #onClientSaslAuthentication(SaslPrincipal, ClientAuthenticationContext)}
+ * are called, or </li>
+ * <li>{@link #onClientAuthenticationFailure(Exception, ClientAuthenticationContext)} is called,
+ * possibly after a call to {@link #onClientTlsAuthentication(X500Principal, ClientAuthenticationContext)}</li>
+ * </ul>
+ * before requests requiring authentication are propagated to the Kafka server.
+ * "Requests requiring authentication" means any Kafka API except for {@code ApiVersions},
+ * {@code SaslHandshake} or {@code SaslAuthenticate}.
+ * </p>
  */
 public interface ClientSubjectAware {
 
     /**
-     * Called when a client authenticates, or reauthenticates, with the proxy.
-     * @param clientSubject The subject that has been authenticated.
+     * Notification of successful TLS authentication of a client.
+     * Called with a non-null {@code clientPrincipal} when a client authenticates, or reauthenticates, with the proxy using a client TLS certificate.
+     * Called with a null {@code clientPrincipal} if:
+     * <ul>
+     *     <li>the connection is not configured to require client TLS authentication and the client presented no TLS certificate,</li>
+     *     <li>if the transport protocol is not TLS</li>
+     * </ul>
+     * @param clientPrincipal The principal that has been authenticated, or null if TLS authentication was not required and not performed.
      * @param context The authentication context.
      */
-    void onClientAuthentication(Subject clientSubject, ClientAuthenticationContext context);
+    void onClientTlsAuthentication(@Nullable X500Principal clientPrincipal,
+                                   ClientAuthenticationContext context);
 
     /**
-     * Called when a client fails authentication, or reauthentication, with the proxy.
+     * Notification of successful SASL authentication of a client.
+     * Called with a non-null {@code clientPrincipal} when a client authenticates, or reauthenticates, with the proxy using SASL.
+     * Called with a null {@code clientPrincipal} if:
+     * <ul>
+     *     <li>SASL authentication is not required and the client progressed to making
+     *      * a request which would require SASL authentication without sending a {@code SaslAuthenticate} request.</li>
+     * </ul>
+     * @param clientPrincipal The principal that has been authenticated, or null if SASL authentication was not required and not performed.
+     * @param context The authentication context.
+     */
+    void onClientSaslAuthentication(@Nullable SaslPrincipal clientPrincipal,
+                                    ClientAuthenticationContext context);
+
+    /**
+     * Notification for failed client authentication outcomes.
+     * Called when a client fails authentication, or reauthentication, with the proxy, for any reason.
      * @param exception The cause of the authentication failure.
      * @param context The authentication context.
      */
-    default void onClientAuthenticationFailure(LoginException exception, ClientAuthenticationContext context) {
+    default void onClientAuthenticationFailure(Exception exception,
+                                               ClientAuthenticationContext context) {
     }
 }
