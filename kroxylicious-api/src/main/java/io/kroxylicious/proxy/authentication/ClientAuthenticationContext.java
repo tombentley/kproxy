726ff66f38cc73bc28e987ab94c1a11e490af2ef
Tom Bentley <tbentley@redhat.com>
2025-07-07 16:52:25 +1200

    ClientSubjectAware
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

---
+++ kroxylicious-api/src/main/java/io/kroxylicious/proxy/authentication/ClientAuthenticationContext.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy.authentication;
+
+import javax.security.auth.Subject;
+
+public interface ClientAuthenticationContext {
+
+    /**
+     * The subject that the proxy presented to the client.
+     * This may be null of the authentication mechanism does not support
+     * mutual authentication.
+     */
+    Subject proxySubject();
+}
---
+++ kroxylicious-api/src/main/java/io/kroxylicious/proxy/authentication/ClientSubjectAware.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy.authentication;
+
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginException;
+
+import io.kroxylicious.proxy.filter.Filter;
+
+/**
+ * This interface may be implemented by {@link Filter}s to learn about client authentication outcomes.
+ */
+interface ClientSubjectAware {
+
+    /**
+     * Called when a client authenticates, or reauthenticates, with the proxy.
+     * @param clientSubject The subject that has been authenticated.
+     * @param context The authentication context.
+     */
+    void onClientAuthentication(Subject clientSubject, ClientAuthenticationContext context);
+
+    /**
+     * Called when a client fails authentication, or reauthentication, with the proxy.
+     * @param exception The cause of the authentication failure.
+     * @param context The authentication context.
+     */
+    default void onClientAuthenticationFailure(LoginException exception, ClientAuthenticationContext context) {
+    }
+}
