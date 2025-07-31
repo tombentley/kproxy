b3dea259b1696261acea8a17f103be78d6ef387a
Tom Bentley <tbentley@redhat.com>
2025-08-01 10:09:48 +1200

    Modularise oauthbearer validation
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

---
+++ kroxylicious-filters/kroxylicious-oauthbearer-validation/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module io.kroxylicious.filter.oauthbearer {
+    requires com.fasterxml.jackson.annotation;
+    requires com.github.benmanes.caffeine;
+    requires com.github.spotbugs.annotations;
+    requires java.security.sasl;
+    requires kafka.clients;
+    requires org.slf4j;
+    requires io.kroxylicious.proxy.tag;
+    requires kroxylicious.api;
+}
