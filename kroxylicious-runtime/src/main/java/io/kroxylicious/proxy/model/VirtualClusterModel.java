eae0f855b352c2fa0339aec8149dad82d35793f5
Tom Bentley <tbentley@redhat.com>
2025-10-31 13:54:39 +1300

    Revert "removing the overloaded virtual cluster ctor"
    This reverts commit cfd1781a6e3708551d47526969ec0be40b6a8473.

--- kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/model/VirtualClusterModel.java
+++ kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/model/VirtualClusterModel.java
@@ -78,6 +78,14 @@ public class VirtualClusterModel {
                                TargetCluster targetCluster,
                                boolean logNetwork,
                                boolean logFrames,
+                               List<NamedFilterDefinition> filters) {
+        this(clusterName, targetCluster, logNetwork, logFrames, filters, null);
+    }
+
+    public VirtualClusterModel(String clusterName,
+                               TargetCluster targetCluster,
+                               boolean logNetwork,
+                               boolean logFrames,
                                List<NamedFilterDefinition> filters,
                                @Nullable SubjectBuilderConfig subjectBuilderConfig) {
         this.clusterName = Objects.requireNonNull(clusterName);
